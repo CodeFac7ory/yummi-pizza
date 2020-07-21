7851cf1429d9639a7b00e398ca7092756cae6d42
Bosko Damjanovic <damjanovicb@googlemail.com>
2020-07-21 18:31:52 +0000

    minor changes in readme and db migration schema

--- database/migrations/2020_07_18_193242_create_pizzas_table.php
+++ database/migrations/2020_07_18_193242_create_pizzas_table.php
@@ -16,7 +16,7 @@ class CreatePizzasTable extends Migration
         Schema::create('pizzas', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
-            $table->string('description', 255);
+            $table->string('description', 1000);
             $table->string('picture', 255);
             $table->integer('price');
             $table->timestamps();
