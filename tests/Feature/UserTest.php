606f4fb63aaf6feb17b26aa5114a482a64ef23ce
Bosko Damjanovic <damjanovicb@googlemail.com>
2020-07-22 09:20:01 +0000

    db CRUD  testing

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -27,16 +27,49 @@ class UserTest extends TestCase
         $response->assertStatus(200);
     }
 
-
     public function testAPIRutes()
     {
+        Log::info('/api/pizzas');
         $response = $this->get('/api/pizzas');
         $response->assertStatus(200);
+        $this->assertCount(7, json_decode($response->getContent())->data);
 
+
+        Log::info('/api/cart');
+        $response = $this->withHeaders([
+            'X-CSRFToken' => 'l7bZRPzepM95zmryTslS484Yp2qrRxI78TNXlHnn',
+        ])->get('/api/cart');
+        Log::info(json_decode($response->getContent()));
+        $response->assertStatus(200);
+        $this->assertCount(0, json_decode($response->getContent()));
+
+        Log::info('/api/orders');
+        $order = [
+            'token' => 'l7bZRPzepM95zmryTslS484Yp2qrRxI78TNXlHnn',
+            'items' => [
+                'pizza_id' => 1,
+                'quantity' => 1,
+                'price' => 799
+            ],
+            'total_price' => 799,
+            'status' => 'pending',
+        ];
+        $response = $this->postJson('/api/orders', $order)->assertStatus(201);
+
+
+        Log::info('/api/cart');
+        $response = $this->withHeaders([
+            'X-CSRFToken' => 'l7bZRPzepM95zmryTslS484Yp2qrRxI78TNXlHnn',
+        ])->get('/api/cart');
+        $response->assertStatus(200);
+        $this->assertCount(1, json_decode($response->getContent()));
+
+
+        Log::info('/api/orders');
         $response = $this->get('/api/orders');
+        Log::info($response->getContent());
         $response->assertStatus(200);
 
-        $response = $this->get('/api/cart');
-        $response->assertStatus(200);
+
     }
 }
